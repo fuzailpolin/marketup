65f79d237cbcdb6d495c3a5949f8d1318f38b2d1
Imran Hossain <imran@algosolver.com>
2021-06-03 16:23:57 +0600

    game lol

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,33 @@
+import {useState, useEffect} from "react";
+
 
 function App() {
-  return (
-    <div>
-      <header className="bg-gray-200">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+
+    const [d, setDiv] = useState([]);
+
+
+    useEffect(() => {
+        let x = []
+        for (let i = 0; i < 20000; i++) {
+            x.push(i)
+        }
+        console.log(x)
+        setDiv(x)
+    }, []);
+
+
+    return (
+        <div>
+            {
+                d.map((v, i) => (
+                    <span key={i} className={'p-2 bg-gray-700 transition rounded-full duration-100 hover:bg-red-900 inline-block'}>
+
+                    </span>
+                )
+                )
+            }
+        </div>
+    );
+};
 
 export default App;
