aa5d379465c71122c99bfecd67e662b8718ca39b
Imran Hossain <imran@algosolver.com>
2021-07-17 14:12:48 +0600

    job crud done

--- components/SubPages/CareerPage/CurrentOpenings/OpeningsDetails/OpeningsDetails.js
+++ components/SubPages/CareerPage/CurrentOpenings/OpeningsDetails/OpeningsDetails.js
@@ -38,7 +38,7 @@ const OpeningsDetails = ({ slug, position, vacancy, jobType, deadLine, link }) =
         <div className={"col-span-12 md:col-span-6 pb-2 md:ml-auto md:mr-0"}>
           <div className={'grid grid-cols-12'}>
             <div className={'col-span-6 '}>
-              <div className="leading-none flex flex-col md:justify-center ">
+              <div className="leading-none flex md:pr-2 flex-col md:justify-center ">
                 <div className="mb-2 text-xs md:text-sm text-gray-400 font-semibold ">
                   Deadline
                 </div>
@@ -47,24 +47,22 @@ const OpeningsDetails = ({ slug, position, vacancy, jobType, deadLine, link }) =
                     "leading-none flex flex-row md:justify-center items-center"
                   }
                 >
-                  <div className="font-semibold text-gray-100 text-xs md:text-sm">
-                    {deadLine?.lastDate}
+                  <div className="font-semiboldtext-gray-100 text-xs md:text-sm">
+                    {new Date(deadLine?.lastDate).toDateString()}
                   </div>
-                  <div className="font-bold text-xs text-gray-400 ml-2">
-                    {deadLine?.daysLeft}
-                  </div>
+
                 </div>
               </div>
             </div>
             <div className={'col-span-6'}>
               <div className={'grid place-items-start md:place-items-center'}>
                 <div className={'flex'}>
-                  <Link href={link?.fb}>
+                  <a href={link?.fb || '#'}>
                     <img className={'mr-2 bg-gray-100 rounded-full p-2 w-9 h-9 cursor-pointer'} src='/Images/Career/facebook.svg' alt='fb not found' />
-                  </Link>
-                  <Link href={link?.insta}>
+                  </a>
+                  <a href={link?.insta || '#'}>
                     <AiOutlineInstagram className={'ml-2 bg-gray-100 text-primary rounded-full p-1 w-9 h-9 cursor-pointer'} />
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
--- layouts/AdminLayout/index.js
+++ layouts/AdminLayout/index.js
@@ -115,14 +115,16 @@ const AdminLayout = ({children}) => {
 
 
                     <li className="my-2 md:my-0">
-                        <a href="#"
-                           className="flex py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
+                        <Link href={'/dashboard/partner'}>
+                            <a
+                                className="flex py-1 md:py-3 pl-1 align-middle text-gray-600 no-underline hover:text-indigo-400">
 
-                            <FaBookOpen className={'inline'}/>
+                                <FaBookOpen className={'inline'}/>
 
-                            <span className="block pl-2 md:pb-0 text-sm left-10">Partnership</span>
+                                <span className="block pl-2 md:pb-0 text-sm left-10">Partnership</span>
 
-                        </a>
+                            </a>
+                        </Link>
                     </li>
 
                     <li className="my-2 md:my-0">
